a7dbc638a075bdcb113935d30db3f3dfd5cd91b7
sukhdhammu <preetkaur7933@gmail.com>
2021-07-24 17:58:19 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+
+n1 = float(input("enter first number"))
+n2 = float(input("enter second number"))
+op = input("enter operation")
+if op == "+":
+    print(n1+n2)
+elif op == "-":
+    print(n1-n2)
+elif op == "*":
+    print(n1*n2)
+elif op == "/":
+    print(n1/n2)
+else:
+    print("Invalid option")
